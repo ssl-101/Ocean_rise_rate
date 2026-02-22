2df5bbc1cc3c4b5db61a6d883397520ff04d8373
ssl-101 <Inquiresiham@gmail.com>
2026-02-22 14:25:24 +0000

    connecting ocean rise level program

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,3 +1,22 @@
+//This Program calculates the ocean levels rise with given years.
 #include <iostream>
+using namespace std;
 
-int main() { std::cout << "Hello Siham!\n"; }
+int main()
+{
+  //Variables for rise rate, 5 years, 7 years and 10 years.
+  const double riseRate = 1.5; // mm per year
+  double rFive = 5;
+  double rSeven = 7;
+  double rTen = 10;
+  //Calculations for ocean level for each year.
+ double fiveYear = riseRate * rFive;
+ double sevenYear = riseRate * rSeven;
+ double tenYear = riseRate * rTen;
+
+  cout <<"The ocean's rise rate for 5 years is " << fiveYear <<"mm per year."<<endl;
+  cout <<"The ocean's rise rate for 7 years is " << sevenYear <<"mm per year."<<endl;
+  cout <<"The ocean's rise rate for 10 years is " << tenYear <<"mm per year."<<endl;
+
+  return 0;
+    }
